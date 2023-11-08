e92444f4f67b5a1f90cf3424ff3edcec5a489367
Mikel Falxa <falxa@apc.in2p3.fr>
2023-11-08 17:24:41 +0100

    add anisotropic gwb simulation

--- fakepta/correlated_noises.py
+++ fakepta/correlated_noises.py
@@ -2,6 +2,7 @@ from .fake_pta import Pulsar
 import numpy as np
 import scipy.constants as sc
 from scipy.interpolate import interp1d
+import healpy as hp
 
 # Misc functions
 def get_correlation(psr_a, psr_b, res_a, res_b):
@@ -40,6 +41,18 @@ def bin_curve(corrs, angles, bins):
     return np.array(mean), np.array(std), np.array(bin_angles)
 
 # ORFs
+def create_gw_antenna_pattern(pos, gwtheta, gwphi):
+
+    m = np.array([np.sin(gwphi), -np.cos(gwphi), np.zeros(len(gwphi))]).T
+    n = np.array([-np.cos(gwtheta) * np.cos(gwphi), -np.cos(gwtheta) * np.sin(gwphi), np.sin(gwtheta)]).T
+    omhat = np.array([-np.sin(gwtheta) * np.cos(gwphi), -np.sin(gwtheta) * np.sin(gwphi), -np.cos(gwtheta)]).T
+
+    fplus = 0.5 * (np.dot(m, pos) ** 2 - np.dot(n, pos) ** 2) / (1 + np.dot(omhat, pos))
+    fcross = (np.dot(m, pos) * np.dot(n, pos)) / (1 + np.dot(omhat, pos))
+    cosMu = -np.dot(omhat, pos)
+
+    return fplus, fcross, cosMu
+
 def hd(psrs):
     orfs = np.zeros((len(psrs), len(psrs)))
     for i in range(len(psrs)):
@@ -50,6 +63,23 @@ def hd(psrs):
                 omc2 = (1 - np.dot(psrs[i].pos, psrs[j].pos)) / 2
                 orfs[i, j] =  1.5 * omc2 * np.log(omc2) - 0.25 * omc2 + 0.5
     return orfs
+
+def anisotropic(psrs, h_map):
+
+    orfs = np.zeros((len(psrs), len(psrs)))
+    npixels = len(h_map)
+    pixels = hp.pix2ang(hp.npix2nside, np.arange(npixels), nest=False)
+    gwtheta = pixels[0]
+    gwphi = pixels[1]
+    for i in range(len(psrs)):
+        for j in range(len(psrs)):
+            if i == j:
+                k_ab = 2.
+            else:
+                k_ab = 1.
+            fp_a, fc_a, _ = create_gw_antenna_pattern(psrs.pos[i], gwtheta, gwphi)
+            fp_b, fc_b, _ = create_gw_antenna_pattern(psrs.pos[j], gwtheta, gwphi)
+            orfs[i, j] = 1.5 * k_ab * np.sum((fp_a*fp_b + fc_a*fc_b) * h_map) / npixels
 
 def monopole(psrs):
     npsr = len(psrs)
@@ -78,7 +108,7 @@ def powerlaw(f, log10_A, gamma):
     return psd_rn
 
 # Noise generating function
-def add_correlated_red_noise_gp(psrs, orf='hd', log10_A=-15., gamma=13/3, rn_components=30, custom_psd=None, f_psd=None):
+def add_correlated_red_noise_gp(psrs, orf='hd', log10_A=-15., gamma=13/3, rn_components=30, custom_psd=None, f_psd=None, h_map=None):
 
     Tspan = np.amax([psr.toas.max() for psr in psrs]) - np.amin([psr.toas.min() for psr in psrs])
     if f_psd is None:
@@ -104,7 +134,10 @@ def add_correlated_red_noise_gp(psrs, orf='hd', log10_A=-15., gamma=13/3, rn_com
             basis_psr[:, 2*i+1] = np.sin(2*np.pi*f[i]*toas)
         basis.append(basis_psr)
     orf_funcs = {'hd':hd, 'monopole':monopole, 'dipole':dipole, 'curn':curn}
-    orfs = orf_funcs[orf](psrs)
+    if orf in [*orf_funcs]:
+        orfs = orf_funcs[orf](psrs)
+    elif orf == 'anisotropic':
+        orfs = anisotropic(psrs, h_map)
     for i in range(len(psrs)):
         for j in range(len(psrs)):
             cov_ij = np.dot(basis[i], np.dot(np.diag(orfs[i, j]*psd_gwb), basis[j].T))
